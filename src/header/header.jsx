7f845381f5ed669079bb63cdc2e6b4b4e147a59f
Alex Bainter <alexbainter@gmail.com>
2021-09-03 17:53:24 -0500

    Add link back to my site

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import classes from './header.module.scss';
 
-const Header = () => <div className={classes.header}>Alex Bainter</div>;
+const LINK_URL = 'https://alexbainter.com';
+
+const Header = () => (
+  <div className={classes.header}>
+    <a href={LINK_URL}>Alex Bainter</a>
+  </div>
+);
 
 export default Header;
