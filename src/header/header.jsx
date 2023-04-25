73b9c733393b30ca69f3b6a027fd7b8826581adc
metalex9 <alexbainter@gmail.com>
2023-04-25 17:44:24 -0500

    Enable header link URL control via URL parameter

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import classes from './header.module.scss';
 
-const LINK_URL = 'https://alexbainter.com';
+const PROFESSIONAL_LINK_CONTROL_PARAM_NAME = 'p';
+const DEFAULT_LINK_URL = 'https://alexbainter.com';
+const PROFESSIONAL_LINK_URL = 'https://alexbainter.dev';
+
+const getLinkUrl = () =>
+  window.location.search && new URLSearchParams(window.location.search).has('p')
+    ? PROFESSIONAL_LINK_URL
+    : DEFAULT_LINK_URL;
 
 const Header = () => (
   <div className={classes.header}>
-    <a href={LINK_URL}>Alex Bainter</a>
+    <a href={getLinkUrl()}>Alex Bainter</a>
   </div>
 );
 
